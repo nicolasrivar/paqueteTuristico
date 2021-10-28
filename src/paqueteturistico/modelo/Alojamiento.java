5cf61c6bce7d1a36b9ab31e5a9113973e0bddbbd
nicolasrivar <nicorivarolamcr@gmail.com>
2021-10-28 17:43:34 -0300

    Initial commit

---
+++ src/paqueteturistico/modelo/Alojamiento.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteturistico.modelo;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Alojamiento {
+    private int idAlojamiento;
+    private Destino Destino;
+    private String ubicacion;
+    private String nombreAlojamiento;
+    private String tipoAlojamiento;
+    private float precioNoche;
+    boolean activo;
+
+    public Alojamiento(Destino Destino, String ubicacion, String nombreAlojamiento, String tipoAlojamiento, float precioNoche, boolean activo) {
+        this.Destino = Destino;
+        this.ubicacion = ubicacion;
+        this.nombreAlojamiento = nombreAlojamiento;
+        this.tipoAlojamiento = tipoAlojamiento;
+        this.precioNoche = precioNoche;
+        this.activo = activo;
+    }
+
+    public Alojamiento() {
+    }
+
+    public Alojamiento(int idAlojamiento, Destino Destino, String ubicacion, String nombreAlojamiento, String tipoAlojamiento, float precioNoche, boolean activo) {
+        this.idAlojamiento = idAlojamiento;
+        this.Destino = Destino;
+        this.ubicacion = ubicacion;
+        this.nombreAlojamiento = nombreAlojamiento;
+        this.tipoAlojamiento = tipoAlojamiento;
+        this.precioNoche = precioNoche;
+        this.activo = activo;
+    }
+
+    public int getIdAlojamiento() {
+        return idAlojamiento;
+    }
+
+    public void setIdAlojamiento(int idAlojamiento) {
+        this.idAlojamiento = idAlojamiento;
+    }
+
+    public Destino getDestino() {
+        return Destino;
+    }
+
+    public void setDestino(Destino Destino) {
+        this.Destino = Destino;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getNombreAlojamiento() {
+        return nombreAlojamiento;
+    }
+
+    public void setNombreAlojamiento(String nombreAlojamiento) {
+        this.nombreAlojamiento = nombreAlojamiento;
+    }
+
+    public String getTipoAlojamiento() {
+        return tipoAlojamiento;
+    }
+
+    public void setTipoAlojamiento(String tipoAlojamiento) {
+        this.tipoAlojamiento = tipoAlojamiento;
+    }
+
+    public float getPrecioNoche() {
+        return precioNoche;
+    }
+
+    public void setPrecioNoche(float precioNoche) {
+        this.precioNoche = precioNoche;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+}
---
+++ src/paqueteturistico/modelo/Menu.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteturistico.modelo;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Menu {
+    private int idMenu;
+    private Alojamiento alojamiento;
+    private String tipoMenu;
+    private int precioMenu;
+    private boolean activo;
+
+    public Menu(int idMenu, Alojamiento alojamiento, String tipoMenu, int precioMenu, boolean activo) {
+        this.idMenu = idMenu;
+        this.alojamiento = alojamiento;
+        this.tipoMenu = tipoMenu;
+        this.precioMenu = precioMenu;
+        this.activo = activo;
+    }
+
+    public Menu(Alojamiento alojamiento, String tipoMenu, int precioMenu, boolean activo) {
+        this.alojamiento = alojamiento;
+        this.tipoMenu = tipoMenu;
+        this.precioMenu = precioMenu;
+        this.activo = activo;
+    }
+
+    public Menu() {
+    }
+
+    public int getIdMenu() {
+        return idMenu;
+    }
+
+    public void setIdMenu(int idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    public Alojamiento getAlojamiento() {
+        return alojamiento;
+    }
+
+    public void setAlojamiento(Alojamiento alojamiento) {
+        this.alojamiento = alojamiento;
+    }
+
+    public String getTipoMenu() {
+        return tipoMenu;
+    }
+
+    public void setTipoMenu(String tipoMenu) {
+        this.tipoMenu = tipoMenu;
+    }
+
+    public int getPrecioMenu() {
+        return precioMenu;
+    }
+
+    public void setPrecioMenu(int precioMenu) {
+        this.precioMenu = precioMenu;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
+    
+    
+}
