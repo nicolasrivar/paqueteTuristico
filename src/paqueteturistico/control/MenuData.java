0147fc75bba749275c9ab5efa64c2c67f7ab71fe
nicolasrivar <nicorivarolamcr@gmail.com>
2021-11-03 10:45:16 -0300

    obtenerMenu

--- src/paqueteturistico/control/MenuData.java
+++ src/paqueteturistico/control/MenuData.java
@@ -6,8 +6,11 @@
 package paqueteturistico.control;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import paqueteturistico.modelo.Conexion;
+import paqueteturistico.modelo.Menu;
 
 /**
  *
@@ -25,6 +28,34 @@ public class MenuData {
             System.out.println("Error en la conexion ");
         }
     
-}
+    }
+    
+   public Menu obtenerMenu(int idMenu){
+       Menu menu=null;
+    
+    String sql="SELECT * FROM menu WHERE idMenu = ?";
+    
+        try {
+            PreparedStatement ps= con.prepareStatement(sql);
+            ps.setInt(1, idMenu);
+            
+            ResultSet rs =ps.executeQuery();
+            if (rs.next()){
+                menu = new Menu();
+                menu.setIdMenu(rs.getInt("idMenu"));
+                menu.setIdAlojamiento(rs.getInt(2));
+                menu.setTipoMenu(rs.getString(3));
+                menu.setPrecioMenu(rs.getFloat(4));
+                menu.setActivo(rs.getBoolean(5));
+            }
+            
+            
+            
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al buscar el menu" + ex);
+        }    
+       return menu;  
+   }
 
 }
